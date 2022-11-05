13d9d13ad8d2643a9229a29a621302c62e3303b9
peatnwr <nwrpeat@gmail.com>
2022-11-05 22:49:39 +0700

    implement activity_main.xml

--- Robot Manual Application/app/src/main/java/com/example/robotmanualapplication/MainActivity.java
+++ Robot Manual Application/app/src/main/java/com/example/robotmanualapplication/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.robotmanualapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton forwardBtn = findViewById(R.id.forwardBtn);
+        FloatingActionButton turnLeftBtn = findViewById(R.id.turnLeftBtn);
+        FloatingActionButton turnRightBtn = findViewById(R.id.turnRightBtn);
+        FloatingActionButton backwardBtn = findViewById(R.id.backwardBtn);
+        Button stopBtn = findViewById(R.id.stopBtn);
+
+        forwardBtn.setOnClickListener(
+                view -> Log.e("forward Button", "Clicked")
+        );
+
+        turnLeftBtn.setOnClickListener(
+                view -> Log.e("turnLeft Button", "Clicked")
+        );
+
+        turnRightBtn.setOnClickListener(
+                view -> Log.e("turnRight Button", "Clicked")
+        );
+
+        backwardBtn.setOnClickListener(
+                view -> Log.e("backward Button", "Clicked")
+        );
+
+        stopBtn.setOnClickListener(
+                view -> Log.e("stop Button", "Clicked")
+        );
+
     }
 }
