d2d4194826bd174f85b202c92c470b4026be16b3
peatnwr <nwrpeat@gmail.com>
2023-01-09 00:41:37 +0700

    edit label.py

--- label.py
+++ label.py
@@ -1,18 +1,15 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('./Thysanoptera GS/259.jpg')
+img = cv2.imread("./Thysanoptera GS/3.jpg")
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-
-contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 for c in contours:
     x, y, w, h = cv2.boundingRect(c)
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    cv2.putText(image, 'Thysanoptera', (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
+    cv2.putText(img, "Thysanoptera", (x, y-10), cv2.FONT_HERSHEY_COMPLEX, 0.3, (255, 0, 0), 2)
 
-cv2.imshow("Image", image)
+cv2.imshow("Labeled Image", img)
 cv2.waitKey(0)
